9e7ad4d7827e219b07d7c639e5e3c5460afb59de
rakeshselvakumar <rakeshselvakumar05@gmail.com>
2026-01-21 14:53:11 +0530

    day 2

---
+++ target_count.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class target_count{
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();
+            int arr[]=new int[n];
+            for(int i=0;i<arr.length;i++){
+                arr[i]=sc.nextInt();
+            }
+            int target=sc.nextInt();
+            int count=0;
+            for(int i=0;i<n;i++){
+                if(arr[i]==target){
+                    count+=1;
+                }
+            }
+            System.out.println("The count of target is:"+count);
+    }
+}
